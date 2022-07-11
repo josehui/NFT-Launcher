1d3f04e70ba559be7070017a6a0270fa47a2c49b
josehui <cshuihui@outlook.com>
2022-07-12 02:34:33 +0800

    add image min

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import viteImagemin from "vite-plugin-imagemin";
 
 const _dirname = path.resolve();
 
@@ -11,7 +12,36 @@ export default defineConfig({
       "@": path.resolve(_dirname, "./src"),
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false,
+          },
+        ],
+      },
+    }),
+  ],
   // build: {
   //   minify: 'terser',
   //   terserOptions: {
